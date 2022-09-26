544982fb8a92535f264fdbdfdd23ea4b84c647b1
Inese Zake <inesa.zake@gmail.com>
2022-09-26 21:40:41 +0300

    Initial commit

---
+++ src/main/java/lekcijaViens/labDarbs/Main.java
@@ -0,0 +1,65 @@
+package lekcijaViens.labDarbs;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //int-integer,satur veselu skaitli
+        int vecums = 34;
+        System.out.println("Mans vecums ir: " + vecums);
+        int temperatura = -10;
+        int garums = 164;
+        System.out.println("Mans garums ir: " + garums);
+        int kurpjuIzmers = 39;
+        System.out.println("Mans kurpjuIzmers ir: " + kurpjuIzmers);
+        int darbaStazs = 19;
+        System.out.println("Mans darbaStazs ir: " + darbaStazs);
+        int svars = 65;
+        System.out.println("Mans svars ir: " + svars);
+        System.out.println("vecums: " + vecums + "\ngarums:" + garums);
+
+        String sunaVards = "Dzonis";
+        boolean vaiIrPotets = true;
+
+
+        String teksts = " Suna vards ir: ";
+        System.out.println(teksts + sunaVards);
+
+        //Maksimālā integer vērtība
+        int maxInt = 2147483647;
+        int minInt = -2147483648;
+        //pienem veselu skaitli no -127 līdz +127
+        byte vecumsDivi = 10;
+
+        short latvijasMazpilseta = 17000;
+
+        long zivisOkeana = 2147483649L;
+
+        float apavuIzmers = 44.5F;
+        System.out.println("apavu izmers: " + apavuIzmers);
+
+        double grauduSvars = 150.3;
+        System.out.println("Graudu svars ir: " + grauduSvars);
+        //Šodien ir sestdiena, lielveikals ir atvērts.
+        boolean isSupermarketOpen = true;
+        // Turpretim skolas ir slēgtas
+        boolean isSchoolOPen = false;
+
+        boolean isFebruaris = false;
+
+
+        String sunaSkirne = "VAS";
+        char valuta = '$';
+        char dzimums = 'V';
+
+        int summa;
+        summa = 5;
+        System.out.println(summa);
+
+
+        int x = 10;
+        int y = 15;
+        int rezultats = x + y;
+        System.out.println(rezultats);
+
+    }
+}
